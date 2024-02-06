e9db277fd8921048531c5a44295a8cb5952b084b
Jrace94 <jfra.ing28@gmail.com>
2024-02-06 16:23:29 -0500

    Task 11: Where can I learn Python? (Update)

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -3,4 +3,4 @@
 
 def schools_by_topic(mongo_collection, topic):
     """Returns a list of dictionaries that contain a topic"""
-    return [doc for doc in mongo_collection.find({'topic': topic})]
+    return mongo_collection.find({"topics": topic})
