5ada7b0df8ffeace0444905b699345fb94ae525c
Jrace94 <jfra.ing28@gmail.com>
2024-04-22 14:40:26 -0500

    Task 2: Basic annotations - floor

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module that presents the floor function with annotations"""
+import math
+
+
+def floor(n: float) -> int:
+    """Function that returns the floor of the float number"""
+    return math.floor(n)
