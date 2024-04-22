6d6e3fa812de56d488b8701d9063561ff3e3ca93
Jrace94 <jfra.ing28@gmail.com>
2024-04-22 14:20:32 -0500

    Task 0: Basic annotations - add

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+"""Module that presents the add function with annotations"""
+
+
 def add(a: float, b: float) -> float:
     """Function that returns the sum of a and b"""
     return a + b
