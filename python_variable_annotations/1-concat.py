6e7e41aeb83fa86d3cfbbe373ff31a42ab46522d
Jrace94 <jfra.ing28@gmail.com>
2024-04-22 14:33:04 -0500

    Task 1: Basic annotations - concat

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module that presents the concat function with annotations"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Function that returns the concatenation of two text strings"""
+    return str1 + str2
