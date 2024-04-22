4d933b75c6d3163a5b21f75fb1920c375dbab32c
Jrace94 <jfra.ing28@gmail.com>
2024-04-22 14:55:27 -0500

    Task 3: Basic annotations - to string

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module that presents the to_str function with annotations"""
+
+
+def to_str(n: float) -> str:
+    """Function that returns the floating number `n` in a text string"""
+    return str(n)
