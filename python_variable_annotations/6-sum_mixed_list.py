05b576f06490bbc9cc4a9bd480cc010cfb75bf9b
Jrace94 <jfra.ing28@gmail.com>
2024-04-22 16:38:04 -0500

    Task 6: Complex types - mixed list

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Module that presents the sum_list function with annotations"""
+from typing import List
+from typing import Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """Function that returns the sum of the numbers in the list `sum_mixed_list`"""
+    return sum(mxd_lst)
