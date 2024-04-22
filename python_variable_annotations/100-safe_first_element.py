704c1be828b3896d5e1758378415557ff0513aa0
Jrace94 <jfra.ing28@gmail.com>
2024-04-22 18:43:42 -0500

    Task 10: Duck typing - first element of a sequence

--- python_variable_annotations/100-safe_first_element.py
+++ python_variable_annotations/100-safe_first_element.py
@@ -2,11 +2,10 @@
 """Module that presents the safe_first_element function with annotations"""
 from typing import Sequence
 from typing import Union
-from typing import Optional
 from typing import Any
 
 
-def safe_first_element(lst: Sequence[Any]) -> Optional[Union[Any, None]]:
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     """Function that returns data of any type"""
     if lst:
         return lst[0]
