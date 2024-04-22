7516b8950c1b97512f6eef4bb6ccf3b120b794f3
Jrace94 <jfra.ing28@gmail.com>
2024-04-22 13:21:53 -0500

    Add README

--- NoSQL/11-schools_by_topic.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-"""School by Topic"""
-
-def schools_by_topic(mongo_collection, topic):
-    """Returns a list of dictionaries that contain a topic"""
-    return mongo_collection.find({"topics": topic})
